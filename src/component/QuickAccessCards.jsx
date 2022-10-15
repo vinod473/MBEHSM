27e3f4039324bff9e8585c293c4e64e879857532
vinod.mahato <vinod.mahato@rapido.bike>
2022-10-15 13:22:19 +0530

    modified cards

--- src/component/QuickAccessCards.jsx
+++ src/component/QuickAccessCards.jsx
@@ -11,42 +11,36 @@ const cardCss = {
 const cardImgCss = {
     maxHeight: '15rem'
 };
+const iconCss = {
+    fontSize: '25px'
+};
 
 const QuickAccessCards = () => {
     return (
         <Container style={{display:'flex', flexWrap:'wrap', justifyContent:'space-around'}}>
             <Card style={cardCss}>
-            <Card.Header style={{textAlign:'center'}}>Downloads</Card.Header>
-            <ListGroup variant="flush">
-                <ListGroup.Item>Application Form</ListGroup.Item>
-                <ListGroup.Item>Examination Form</ListGroup.Item>
-                <ListGroup.Item>Syllabus Download</ListGroup.Item>
-                <ListGroup.Item>Brochure Download</ListGroup.Item>
-                <ListGroup.Item>Practice Registration Form</ListGroup.Item>
-                <ListGroup.Item>Download Student's Admit Card</ListGroup.Item>
-            </ListGroup>
+                <Card.Header style={{textAlign:'center'}}>
+                    <i style={iconCss} class="bi bi-cloud-arrow-down-fill" />
+                    Downloads
+                </Card.Header>
+                <ListGroup variant="flush">
+                    <ListGroup.Item><i class="bi bi-download"></i>Application Form</ListGroup.Item>
+                    <ListGroup.Item><i class="bi bi-download"></i>Examination Form</ListGroup.Item>
+                    <ListGroup.Item><i class="bi bi-download"></i>Syllabus Download</ListGroup.Item>
+                    <ListGroup.Item><i class="bi bi-download"></i>Brochure Download</ListGroup.Item>
+                    <ListGroup.Item><i class="bi bi-download"></i>Practice Registration Form</ListGroup.Item>
+                    <ListGroup.Item><i class="bi bi-download"></i>Download Student's Admit Card</ListGroup.Item>
+                </ListGroup>
             </Card>
             <Card style={cardCss}>
-                <Card.Img style={cardImgCss} variant="top" src="pexels-lil-artsy-1925536.jpg" />
-                <Card.Body>
-                    <Card.Title>Electro Homeopathy</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Read More</Button>
-                </Card.Body>
+                <Card.Header style={{textAlign:'center'}}>
+                    Notice Board
+                </Card.Header>
             </Card>
             <Card style={cardCss}>
-                <Card.Img style={cardImgCss} variant="top" src="pexels-perfecto-capucine-1329571.jpg" />
-                <Card.Body>
-                    <Card.Title>Course Details</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Read More</Button>
-                </Card.Body>
+                <Card.Header style={{textAlign:'center'}}>
+                    College Login Dashboard
+                </Card.Header>
             </Card>
             <Card style={cardCss}>
                 <Card.Img style={cardImgCss} variant="top" src="pexels-gustavo-fring-3985153.jpg" />
