439ccd0bebcd87848b5df1a7b89bd203951435d7
vinod.mahato <vinod.mahato@rapido.bike>
2022-10-15 13:43:12 +0530

    added icons and blinker in contact us header

--- src/component/ContactComponent.jsx
+++ src/component/ContactComponent.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import '../App.css';
 
 const contactCss = {
     backgroundColor: 'lightgreen'
 };
-const hello = {
+const contactItemCss = {
     padding: '5px',
 };
+const iconCss = {
+    margin: '0px 8px'
+};
+
 const ContactComponent = () => {
     return (
         <div style={contactCss}>
             <Container>
-                <span style={hello}>
+                <span style={contactItemCss}>
+                    <i style={iconCss} class="bi bi-telephone-fill"/>
                     +91-7599833082,9458445207
                 </span>
-                <span style={hello}>admission@mbehsmindia.com</span>
-                <span style={hello}>Welcome to our website</span>
+                <span style={contactItemCss}>
+                    <i style={iconCss} class="bi bi-envelope-fill" />
+                    admission@mbehsmindia.com
+                </span>
+                <span style={contactItemCss}>
+                    <i style={iconCss} class="bi bi-hand-index-thumb-fill" />
+                    <span class='blink_me'>Welcome to our website</span>
+                </span>
             </Container>
         </div>
     );
