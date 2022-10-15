2e24c49862786571f212094dbee84e7a92471410
vinod.mahato <vinod.mahato@rapido.bike>
2022-10-15 09:25:15 +0530

    added project boiler plate code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactComponent from './component/ContactComponent';
+import NavbarComponent from './component/NavbarComponent';
+
+function App() {
+  return (
+    <header>
+      <ContactComponent />
+      <NavbarComponent />
+    </header>
+  );
+}
+
+export default App;
